4fbfab412b9876c29736501d3a788ba8e8675dec
kvrathan2718@gmail.com <03rathanv@users.noreply.github.com>
2025-11-17 00:23:43 +0530

    Fix views and router

--- movies/urls.py
+++ movies/urls.py
@@ -1,13 +1,13 @@
-# movies/urls.py
 from django.urls import path, include
-from . import views
+from rest_framework.routers import DefaultRouter
+from .views import MovieViewSet, ReviewViewSet
+from . import auth_urls
+
+router = DefaultRouter()
+router.register("movies", MovieViewSet, basename="movies")
+router.register("reviews", ReviewViewSet, basename="reviews")
 
 urlpatterns = [
-    path("", views.movie_list),
-    path("<int:pk>/", views.movie_detail),
-    path("reviews/", views.review_create),
-    path("reviews/<int:pk>/react/", views.review_react),
-
-    # AUTH ROUTES HERE
-    path("auth/", include("movies.auth_urls")),
+    path("", include(router.urls)),
+    path("auth/", include(auth_urls)),
 ]
